244d873026d760a2ff26d4ec30a55d8d2a140464
Bjorn Persmats <bjorn.pm@plankter.se>
2015-12-16 09:23:03 +0100

    Added timestamp and real-time camera support in main. Also added distances to chessboard pattern for camera

--- vehicle_detection/main.cpp
+++ vehicle_detection/main.cpp
@@ -16,6 +16,8 @@
 #include <ctime>
 #include <fstream>
 #include "DistanceEstimation.h"
+#include <time.h>
+
 
 using namespace std;
 using namespace cv;
@@ -25,15 +27,19 @@ const char * haarClassPath = "haar_classifiers/currently_used_classifier.xml";
 const char * haarClassPath1 = "haar_classifiers/cars3.xml";
 const char * svmPath = "trained_svm.xml";
 const char * normConstPath = "svm_normalization_const.csv";
-bool exportHaarImages = false;
 string haarImgExport = "tmp_img/";
+string videoOutputPath = "recordedOutput.avi";
+string videoSourcePath = "recordedSource.avi";
+const std::string videoStreamAddress = "http://root:pass@192.168.0.90/axis-cgi/mjpg/video.cgi?user=USERNAME&password=PWD&channel=0&.mjpg";
+const char * timestampPath= "timestamps.log"; 
+
+bool exportHaarImages = false;
 int haarExportCounter = 0;
 int tmpCounter = 0;
 
 int main(int argc, const char * argv[]) {
   cout << "\n";
 
-  CvCapture* capture;
 
 
   const char * videoPath = "defaultPath";
@@ -45,12 +51,21 @@ int main(int argc, const char * argv[]) {
   }
   cout << "Path to trained SVM: " << svmPath <<  "\n";
   
+  CvCapture* capture;
   capture = cvCreateFileCapture(videoPath);
 
-  Mat src,IMG,imageROI;
+  /*
+  VideoCapture capture;
+  if(!capture.open(videoStreamAddress)) {
+    cout << "Error opening video stream or file" << endl;
+    return -1;
+  }
+  */
+
+  Mat src,IMG,imageROI,ORIGSOURCE;
 
   // Load Haar classifier (used for generating hypotheses)
-  CascadeClassifier haarClassifier = CascadeClassifier(haarClassPath1);
+  CascadeClassifier haarClassifier = CascadeClassifier(haarClassPath);
   if (haarClassifier.empty()) {
     cout << "LOADED HAAR CLASSIFIER WAS EMPTY!!!\n";
   }
@@ -79,31 +94,43 @@ int main(int argc, const char * argv[]) {
     //cout << rescalingConstants[i][1] << "\n";
   }
 
+  // time stamp stuff
+  time_t raw_time;
+  ofstream timestampFile;
+  timestampFile.open(timestampPath);
+  timestampFile << ctime(&raw_time) << "\n\n";
 
-  int windowWidth = 800;
-  int windowHeight = 600;
+
+  int windowWidth = 640;
+  int windowHeight = 480;
 
   vector<Rect> detectedVehicles;
   int roiY = 200;
   Rect regionOfInterest(0, roiY, windowWidth, windowHeight-roiY);
 
   initializeDistanceEstimation();
+
+  //VideoWriter vidWriter(videoOutputPath, CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(windowWidth, windowHeight), true);
+  //VideoWriter vidSourceWriter(videoSourcePath, CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(windowWidth, windowHeight), true);
   
   cout << "Just before video loop \n";
+  time(&raw_time);
+  clock_t begin = clock();
   while(1) {
     
     //cout << "Inside video loop \n";
     // Get frame
-    int frameSkip = 5;
+    int frameSkip = 1;
     for (int i=0; i<frameSkip; i++) {
       src = cvQueryFrame(capture);
     }
+    //capture.read(src);
     resize(src, src, Size(windowWidth,windowHeight),0,0,INTER_CUBIC);
     IMG = src.clone();
+    ORIGSOURCE = src.clone();
     //resize(src, src, Size(320,240),0,0,INTER_CUBIC);
 
     imageROI = src(regionOfInterest);
-    clock_t begin = clock();
     // Process frame
 
     GaussianBlur(src, src, Size(3,3), 1);
@@ -113,7 +140,7 @@ int main(int argc, const char * argv[]) {
     //waitKey(0);
     //cout << "Before detectMultiScale  \n";
     //haarClassifier.detectMultiScale(imageROI, detectedVehicles);
-    //haarClassifier.detectMultiScale(imageROI, detectedVehicles, 1.01, 1, 0 | CASCADE_FIND_BIGGEST_OBJECT, Size(5, 5), Size(150, 150));
+    //haarClassifier.detectMultiScale(imageROI, detectedVehicles, 1.03, 3, 0 | CASCADE_FIND_BIGGEST_OBJECT, Size(10, 10), Size(150, 150));
     haarClassifier.detectMultiScale(imageROI, detectedVehicles, 1.03, 1, 0, Size(10, 10), Size(150, 150));
     //haarClassifier.detectMultiScale(imageROI, detectedVehicles, 1.1, 3, 0 | CASCADE_FIND_BIGGEST_OBJECT, Size(20, 20), Size(150, 150));
     //cout << "Just before for loop \n";
@@ -156,7 +183,7 @@ int main(int argc, const char * argv[]) {
         it++;
         idx++;
       }
-      cout << "After loop \n";
+      //cout << "After loop \n";
       /*
       if (abs(featureVector[140])>100 && abs(featureVector[141])>100 && abs(featureVector[142])>100) {
         cout << "This is a picture that gives weird numbers\n";
@@ -181,8 +208,8 @@ int main(int argc, const char * argv[]) {
       // The following line will crash if wrong number of inputs to SVM!
       float response = mySVM.predict(featureMat);
       //float response = 0;
-      cout << "SVM response: " << response << "\n";
-      imshow("debug", src(r));
+      //cout << "SVM response: " << response << "\n";
+      //imshow("debug", src(r));
       //waitKey(0);
 
       if (response > 0) {
@@ -197,7 +224,7 @@ int main(int argc, const char * argv[]) {
       ostringstream mySs;
       mySs << estDist << " m";
       string distText = mySs.str();
-      putText(IMG, distText.c_str(), Point(r.x, r.y), FONT_HERSHEY_PLAIN, 1, Scalar(255, 255, 255));
+      //putText(IMG, distText.c_str(), Point(r.x, r.y), FONT_HERSHEY_PLAIN, 1, Scalar(255, 255, 255));
     }
     
     //cout<<(float)(clock()-begin) / CLOCKS_PER_SEC<<endl;
@@ -209,13 +236,21 @@ int main(int argc, const char * argv[]) {
     imshow("Vehicle detection", IMG);
     moveWindow("Vehicle detection", 100, 0);
 
+    // Save to video
+    //vidWriter.write(IMG);
+    //vidSourceWriter.write(ORIGSOURCE);
+
+    float timeTaken = (float) (clock()-begin)/CLOCKS_PER_SEC;
+    timestampFile << timeTaken << "\t" << "\n";
+
     // Key press events
     char key = (char)waitKey(1); //time interval for reading key input;
     if(key == 'q' || key == 'Q' || key == 27)
       break;
   }
   cout << "Just after video loop \n";
-  cvReleaseCapture(&capture);
+  //cvReleaseCapture(&capture);
   cvDestroyWindow("Vehicle detection");
+  timestampFile.close();
   return 0;
 }
