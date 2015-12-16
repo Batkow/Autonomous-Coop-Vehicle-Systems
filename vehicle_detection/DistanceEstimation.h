0f96cec15898291a7f9dd5f69c65c4b1d6be1bf4
Bjorn Persmats <bjorn.pm@plankter.se>
2015-12-16 21:02:44 +0100

    Distance estimation works reasonably well

--- vehicle_detection/DistanceEstimation.h
+++ vehicle_detection/DistanceEstimation.h
@@ -1,6 +1,7 @@
 
 #pragma once
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -16,5 +17,10 @@ void initializeDistanceEstimation() {
 
 
 float estimateDistance(float camXCoord, float camYCoord) {
-	return 3.14;
+	float h = 1.7410826179;
+	float c = 0.00144671;//1.0/691.2256;
+	float r = 1.24896677;//3.141593/2.5154;
+	float output = h*tan(c*camYCoord + r);
+	//cout << "Distance is: " << output << "\n";
+	return output;
 }
